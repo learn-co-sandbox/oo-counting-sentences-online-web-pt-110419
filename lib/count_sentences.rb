114660f7dba1f99813c81aba0ae2d415e86e496c
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-01-04 01:31:02 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,33 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
-    
+    word = {}
+    sentence?.each_with do |arr|
+      if arr.map do |period|
+        word.end_with?(".")
+        
+      end 
+      return true
+    end 
+      # word << arr.split(/\w+/)
+    end 
+   # self =~ end_with?(".")
+  # scan(/\w+/).count 
+
   end
 
   def question?
-
+     
   end
 
   def exclamation?
-
+      
   end
 
   def count_sentences
-
+    split()
+    # scan(/\w+\).count
   end
 end
